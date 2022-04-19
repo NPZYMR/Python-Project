5f97804005bd9ea3fd82593a50965d295302ff5f
Naveen Pandia <naveen.pandia@zymr.com>
2022-04-19 11:57:21 +0530

    First File

---
+++ index.py
@@ -0,0 +1,10 @@
+def max_num_in_list(list):
+    max = list[0]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+
+
+
+print(max_num_in_list([5,7,3,1000,-1]))                
